ad5b668d00085e07138e9fbc3eec4b4237ed1b19
Klork-droid <60038225+Klork-droid@users.noreply.github.com>
2020-02-18 09:08:16 +0300

    upgrade django for heroku

--- tga/tga/settings.py
+++ tga/tga/settings.py
@@ -11,9 +11,18 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'myappfolder/static'),
+)
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -107,4 +116,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+django_heroku.settings(locals())
